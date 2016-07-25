38626dc2391aa9495873f72f16f57a867883a0ab
Brett New and Julia Dickey <brett+julia@>
2016-07-25 13:27:55 -0700

    Create and pass all specs for Tag class.

--- spec/skill_spec.rb
+++ spec/skill_spec.rb
@@ -10,4 +10,13 @@ describe(Skill) do
     skill = Skill.new({:description => ("a" * 19)})
     expect(skill.save).to(eq(false))
   end
+
+  it('returns the tags associated with the skill') do
+    skill = Skill.create({:description => "pogo stick racing"})
+    tag = Tag.create({:name => "sports"})
+    tag2 = Tag.create({:name => "health"})
+    skill.tags.push(tag)
+    skill.tags.push(tag2)
+    expect(skill.tags).to(eq([tag, tag2]))
+  end
 end
